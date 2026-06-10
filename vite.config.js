b9ae43231e255de1074429041c2ea531eac7443d
nirraschi <nirraschi@outlook.com>
2026-06-10 19:48:40 -0300

    vite iamges

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
 plugins: [
 tailwindcss(),
+ViteImageOptimizer({
+      jpg: { quality: 75 },
+      jpeg: { quality: 75 },
+      png: { quality: 75 },
+      webp: { quality: 75 },
+    }),
 ],
 })
